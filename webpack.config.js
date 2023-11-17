bd14fe878397fb4ea34d8488043967ea2d2a826a
leonid Kozlovskiy <lenya.kozlovskiy.97@mail.ru>
2023-11-17 07:10:29 +0300

    feat: trying to undestanding webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   entry: path.resolve(__dirname, "coffee-house", "index.js"),
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     clean: true,
     filename: "[name].[contenthash].js",
   },
