3e5bd3c9bf78423773a8dd7d0cf0ac2691f0e914
leonid Kozlovskiy <lenya.kozlovskiy.97@mail.ru>
2023-11-17 03:56:43 +0300

    feat:add descrtiption of my work

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -1,8 +1,8 @@
+console.log(
+  "1.Checking validation of pages: +16\n2.The layout matches the design: +42\n3.CSS Requirements: +10\n4.Interactivity: +32 "
+);
 import "./index.html";
 import "./page2.html";
 import "./main.scss";
 import "./modules/bar.js";
 import "./modules/buttons.js";
-
-const sum = (a, b) => a + b;
-console.log(sum(5, 10));
