2c7cb9f31da08a7f0d4b2f0966295e9be00fbee0
leonid Kozlovskiy <lenya.kozlovskiy.97@mail.ru>
2023-11-17 03:48:59 +0300

    add all interactive

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -2,6 +2,7 @@ import "./index.html";
 import "./page2.html";
 import "./main.scss";
 import "./modules/bar.js";
+import "./modules/buttons.js";
 
 const sum = (a, b) => a + b;
 console.log(sum(5, 10));
