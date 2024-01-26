099ce4242f7e7610fb3c106e5394237011c2f279
leonid Kozlovskiy <lenya.kozlovskiy.97@mail.ru>
2024-01-26 10:55:21 +0300

    feat:add logic start game

--- src/modules-js/html.js
+++ src/modules-js/html.js
@@ -27,26 +27,50 @@ const nonogrammContainer = createElement({
 });
 const topLeft = createElement({
   tag: "div",
-  classes: ["container__top-left"],
+  classes: ["container", "container__top-left"],
   text: "",
 });
 const topRight = createElement({
   tag: "div",
-  classes: ["container__letf-calc"],
+  classes: ["container", "container__top-right"],
   text: "",
 });
 const bottomLeft = createElement({
   tag: "div",
-  classes: ["container__bottom-calc"],
+  classes: ["container", "container__bottom-left"],
   text: "",
 });
 
 const bottomRight = createElement({
   tag: "div",
-  classes: ["container__right-bottom"],
+  classes: ["container", "container__bottom-right"],
   text: "",
 });
 
+// const game = [
+//   [0, 0, 1, 0, 0],
+//   [0, 1, 0, 1, 0],
+//   [1, 1, 1, 1, 1],
+//   [0, 1, 0, 1, 0],
+//   [0, 0, 1, 0, 0],
+// ];
+const game = [
+  [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0],
+  [0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0],
+  [0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0],
+  [1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0],
+  [0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1],
+  [0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1],
+  [0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+];
 // append
 body.append(main);
 main.append(wrapper);
@@ -57,6 +81,7 @@ nonogrammContainer.append(topLeft);
 nonogrammContainer.append(topRight);
 nonogrammContainer.append(bottomLeft);
 nonogrammContainer.append(bottomRight);
+
 // function
 function createElement(options) {
   const { tag = "div", text = "", parent, classes = [] } = options;
@@ -70,28 +95,95 @@ function createElement(options) {
   }
   return element;
 }
-function createColumns(n, parent) {
-  const result = [];
-  for (let i = 0; i < n; i++) {
+function createColumns(height, width, parent, purpose, value) {
+  let item = [];
+  let className = ["column"];
+  if (purpose === "top") {
+    className = ["column", "column-top"];
+  } else if (purpose === "right") {
+    className = ["column", "column-right"];
+  } else {
+    className = ["column", "column-left"];
+  }
+
+  for (let i = 0; i < height; i++) {
     const columns = createElement({
       tag: "div",
-      classes: ["column"],
+      classes: className,
     });
-    result.push(columns);
-    parent.append(columns);
-    // createItem(columns);
+    if (parent) {
+      parent.append(columns);
+    }
+    item.push(createItem(columns, width, purpose, value));
+  }
+  return item;
+}
+
+function createItem(column, row, purpose, value) {
+  const result = [];
+  value = "";
+  let className = ["item"];
+  if (purpose === "top") {
+    className.push("item-top");
+  } else if (purpose === "right") {
+    className.push("item-right");
+  } else {
+    className.push("item-left");
+  }
+  for (let i = 0; i < row; i++) {
+    const items = createElement({
+      tag: "div",
+      classes: className,
+      text: value,
+    });
+    result.push(items);
+    column.append(items);
   }
   return result;
 }
-function createItem(column) {
-  const result = [];
-  for (let i = 0; i < 5; i++) {
-    const items = createElement({
-      tag: "div",
-      classes: ["item"],
+
+function createGame(game) {
+  const gameValue = document.querySelectorAll(".item-left");
+  const gameBoard = createColumns(
+    game.length,
+    game.length,
+    bottomRight,
+    "right"
+  );
+  const searchLeft = createColumns(game.length, game.length, bottomLeft);
+  const seachTop = createColumns(game.length, game.length, topRight, "top");
+  for (let i = 0; i < game.length; i++) {
+    let countLeft = 0;
+    let countRight = 0;
+    let arrCountLeft = [];
+    let arrCountRight = [];
+    for (let j = 0; j < game.length; j++) {
+      if (game[i][j] === 1) {
+        countLeft++;
+        gameBoard[i][j].textContent = game[i][j];
+      } else if (countLeft > 0) {
+        arrCountLeft.push(countLeft);
+        countLeft = 0;
+      }
+      if (game[j][i] === 1) {
+        countRight++;
+      } else if (countRight > 0) {
+        arrCountRight.push(countRight);
+        countRight = 0;
+      }
+    }
+    if (countLeft > 0) {
+      arrCountLeft.push(countLeft);
+    }
+    if (countRight > 0) {
+      arrCountRight.push(countRight);
+    }
+    arrCountRight.forEach((e, j) => {
+      seachTop[i][j].textContent = e;
     });
-    column.append(items);
+    arrCountLeft.forEach((e, j) => {
+      searchLeft[i][j].textContent = e;
+    });
   }
 }
-createColumns(5, topLeft);
-console.log(createColumns(5, topLeft));
+createGame(game);
