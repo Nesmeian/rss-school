91e1de3e30e6d09109f78025e682b7623500aef1
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-02-25 15:37:14 +0300

    refactor:change apiUrl

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super(('https://newsapi.org/v2/' as string), {
+        super(('https://rss-news-api.onrender.com/' as string), {
             apiKey: '992e8d96108d477e8604b655b740bd2f'
         });
     }
