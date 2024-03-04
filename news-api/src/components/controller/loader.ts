95c2b64da2a4990067b1c88b5c8320a01079f1e9
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-03-04 16:38:49 +0300

    feat: footer now fixed in bottom, also prettier now working

--- news-api/src/components/controller/loader.ts
+++ news-api/src/components/controller/loader.ts
@@ -4,27 +4,27 @@ interface UrlRes {
     redirected: boolean;
     status: number;
     ok: boolean;
-    statusText:string;
-    json:()=>Promise <JSON>;
+    statusText: string;
+    json: () => Promise<JSON>;
 }
 class Loader {
-    baseLink:string;
-    options:object;
-    constructor(baseLink:string, options:object) {
+    baseLink: string;
+    options: object;
+    constructor(baseLink: string, options: object) {
         this.baseLink = baseLink;
         this.options = options;
     }
-    
+
     public getResp(
-        { endpoint, options = {} }:{endpoint:string,options?:object},
+        { endpoint, options = {} }: { endpoint: string; options?: object },
         callback = () => {
             console.error('No callback for GET response');
         }
-    ):void {
+    ): void {
         this.load('GET', endpoint, callback, options);
     }
 
-    private errorHandler(res:UrlRes) {
+    private errorHandler(res: UrlRes) {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -33,7 +33,7 @@ class Loader {
         return res;
     }
 
-    private makeUrl(options: RequestOptions, endpoint: string):string {
+    private makeUrl(options: RequestOptions, endpoint: string): string {
         const urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
@@ -53,6 +53,6 @@ class Loader {
     }
 }
 type RequestOptions = {
-    [key: string]: string
-  }
+    [key: string]: string;
+};
 export default Loader;
