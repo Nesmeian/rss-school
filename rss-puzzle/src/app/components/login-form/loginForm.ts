d66a2982f97afc28757b61b7c524ace3b82398e7
леонид <lenya.kozlovskiy.97@mail.ru>
2024-03-09 15:36:08 +0300

    Feat/rss pz 02 implement comprehensive validation (#127)
    * init:start task and setting webpack
    
    * feat:task complete

--- rss-puzzle/src/app/components/login-form/loginForm.ts
+++ rss-puzzle/src/app/components/login-form/loginForm.ts
@@ -1,6 +1,7 @@
-import createElement, {Html} from '../../../utlils/createFunction'
-import './loginForm.scss'
 
+import createElement, { Html } from '../../../utlils/createFunction';
+import './loginForm.scss';
+type VariableType = 'name' | 'surname';
 export class FormLogin {
   form: Html
   constructor() {
@@ -12,28 +13,32 @@ export class FormLogin {
   formCreate() {
     const container = this.createLoginForm()
 
-    const nameLabel = this.createLoginNameLabel()
-    const name: any = this.createLoginName()
-    nameLabel.append(name)
-    const surnameLabel = this.createLoginSurnameLabel()
-    const surname: any = this.createLoginSurname()
+        const nameLabel = this.createLoginNameLabel();
+        const name: Element | HTMLInputElement = this.createLoginName();
+        nameLabel.append(name);
+        const surnameLabel = this.createLoginSurnameLabel();
+        const surname: Element | HTMLInputElement = this.createLoginSurname();
 
-    surnameLabel.append(surname)
-    const button = this.createLoginButton()
-    container.append(nameLabel)
-    container.append(surnameLabel)
-    container.append(button)
-    container.addEventListener('submit', (e) => {
-      e.preventDefault()
-      console.log(5)
-    })
-    return container
-  }
-  createLoginForm() {
-    const param = {
-      tag: 'form',
-      classes: ['register__form'],
-      text: '',
+        surnameLabel.append(surname);
+        const button = this.createLoginButton();
+        container.append(nameLabel);
+        container.append(surnameLabel);
+        container.append(button);
+
+        container.addEventListener('submit', (e) => {
+            e.preventDefault();
+            this.removeMessage();
+            const notValideMessageName = this.createValidationMessage();
+            const notValideMessageSurname = this.createValidationMessage();
+            if (!this.validation(name, 3, notValideMessageName)) {
+                nameLabel.append(notValideMessageName);
+            }
+            if (!this.validation(surname, 4, notValideMessageSurname)) {
+                surnameLabel.append(notValideMessageSurname);
+            }
+        });
+        return container;
+
     }
     const element = new createElement(param).getElement()
     element.setAttribute('action', '')
@@ -47,14 +52,16 @@ export class FormLogin {
       classes: ['label', 'register__name-label'],
       text: 'Name',
     }
-    let element = new createElement(param).getElement()
-    return element
-  }
-  createLoginName() {
-    const param = {
-      tag: 'input',
-      classes: ['input', 'register__name'],
-      text: '',
+    createLoginName() {
+        const param = {
+            tag: 'input',
+            classes: ['input', 'register__name'],
+            text: '',
+        };
+        const element = new createElement(param).getElement() as HTMLInputElement;
+        element.setAttribute('type', 'text');
+        return element;
+
     }
     const element = new createElement(param).getElement()
     element.setAttribute('type', 'text')
@@ -69,14 +76,16 @@ export class FormLogin {
       classes: ['label', 'register__surname-label'],
       text: 'Surname',
     }
-    const element = new createElement(param).getElement()
-    return element
-  }
-  createLoginSurname() {
-    const param = {
-      tag: 'input',
-      classes: ['input', 'register__surname'],
-      text: '',
+    createLoginSurname() {
+        const param = {
+            tag: 'input',
+            classes: ['input', 'register__surname'],
+            text: '',
+        };
+
+        const element = new createElement(param).getElement();
+        element.setAttribute('type', 'text');
+        return element;
     }
 
     const element = new createElement(param).getElement()
@@ -92,8 +101,42 @@ export class FormLogin {
       classes: ['register__button'],
       text: 'Enter',
     }
-    const element = new createElement(param).getElement()
-    element.setAttribute('type', 'submit')
-    return element
-  }
+    createValidationMessage() {
+        const param = {
+            tag: 'div',
+            classes: ['form__message'],
+        };
+        const element = new createElement(param).getElement();
+        return element;
+    }
+    validation(input: Element | HTMLInputElement, minLeng: number, message: Html) {
+        let condition = false;
+        if (input instanceof HTMLInputElement) {
+            let result = input.value.split('');
+            let minLength = minLeng;
+            if (result.length >= minLength) {
+                if (result[0] === result[0].toUpperCase()) {
+                    result.forEach((e: string, i: number) => {
+                        if (/^[A-Z][a-zA-Z-]*$/.test(e)) {
+                            input.classList.add('input--active');
+                            condition = true;
+                        }
+                    });
+                } else {
+                    input.classList.add('input--inactive');
+                    message.textContent = 'The input is not valid, the first letter must be capitalized ';
+                }
+            } else {
+                input.classList.add('input--inactive');
+                message.textContent = 'The input is not valid is too short';
+            }
+        }
+        return condition;
+    }
+    removeMessage() {
+        const errorMessage = document.querySelectorAll('.form__message');
+        errorMessage.forEach((elem) => {
+            elem.remove();
+        });
+    }
 }
--- rss-puzzle/webpack.config.js
+++ rss-puzzle/webpack.config.js
@@ -6,25 +6,38 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const DotenvWebpackPlugin = require('dotenv-webpack')
 
 const baseConfig = {
-  entry: path.resolve(__dirname, './src/index'),
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.ts$/i,
-        use: 'ts-loader',
-      },
-      {
-        test: /\.woff2?$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name][ext]',
-        },
-      },
+    entry: path.resolve(__dirname, './src/index'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.ts$/i,
+                use: 'ts-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './rss-puzzle'),
+        clean: true,
+        assetModuleFilename: 'assets/[hash][ext]',
+    },
+    plugins: [
+        new DotenvWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new EslingPlugin({ extensions: 'ts' }),
+        new CleanWebpackPlugin(),
+
     ],
   },
   resolve: {
